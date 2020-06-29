84f4d086161bc8436f672d6e4aa20f8995bb3607
伟 黄 <1316279031@qq.com>
2020-06-29 21:54:42 +0800

    2020-6-29

---
+++ StudentInformationManagerSystem/StudentInformationManagerSystem/Form1.Designer.cs
@@ -0,0 +1,295 @@
+﻿namespace StudentInformationManagerSystem
+{
+    partial class FrmLog
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ucTextBoxEx1 = new HZH_Controls.Controls.UCTextBoxEx();
+            this.ucSplitLabel1 = new HZH_Controls.Controls.UCSplitLabel();
+            this.ucSplitLabel2 = new HZH_Controls.Controls.UCSplitLabel();
+            this.ucTextBoxEx2 = new HZH_Controls.Controls.UCTextBoxEx();
+            this.ucBtnExt1 = new HZH_Controls.Controls.UCBtnExt();
+            this.textBoxTransparent1 = new HZH_Controls.Controls.TextBoxTransparent();
+            this.ucTextBoxEx3 = new HZH_Controls.Controls.UCTextBoxEx();
+            this.ucSplitLabel3 = new HZH_Controls.Controls.UCSplitLabel();
+            this.SuspendLayout();
+            // 
+            // ucTextBoxEx1
+            // 
+            this.ucTextBoxEx1.BackColor = System.Drawing.Color.Transparent;
+            this.ucTextBoxEx1.ConerRadius = 5;
+            this.ucTextBoxEx1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.ucTextBoxEx1.DecLength = 2;
+            this.ucTextBoxEx1.FillColor = System.Drawing.Color.Empty;
+            this.ucTextBoxEx1.FocusBorderColor = System.Drawing.Color.White;
+            this.ucTextBoxEx1.Font = new System.Drawing.Font("微软雅黑", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.ucTextBoxEx1.InputText = "";
+            this.ucTextBoxEx1.InputType = HZH_Controls.TextInputType.NotControl;
+            this.ucTextBoxEx1.IsFocusColor = true;
+            this.ucTextBoxEx1.IsRadius = true;
+            this.ucTextBoxEx1.IsShowClearBtn = true;
+            this.ucTextBoxEx1.IsShowKeyboard = false;
+            this.ucTextBoxEx1.IsShowRect = false;
+            this.ucTextBoxEx1.IsShowSearchBtn = false;
+            this.ucTextBoxEx1.KeyBoardType = HZH_Controls.Controls.KeyBoardType.UCKeyBorderAll_EN;
+            this.ucTextBoxEx1.Location = new System.Drawing.Point(150, 69);
+            this.ucTextBoxEx1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.ucTextBoxEx1.MaxValue = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            this.ucTextBoxEx1.MinValue = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            -2147483648});
+            this.ucTextBoxEx1.Name = "ucTextBoxEx1";
+            this.ucTextBoxEx1.Padding = new System.Windows.Forms.Padding(5);
+            this.ucTextBoxEx1.PromptColor = System.Drawing.Color.Gray;
+            this.ucTextBoxEx1.PromptFont = new System.Drawing.Font("微软雅黑", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.ucTextBoxEx1.PromptText = "";
+            this.ucTextBoxEx1.RectColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(220)))));
+            this.ucTextBoxEx1.RectWidth = 1;
+            this.ucTextBoxEx1.RegexPattern = "";
+            this.ucTextBoxEx1.Size = new System.Drawing.Size(273, 42);
+            this.ucTextBoxEx1.TabIndex = 0;
+            // 
+            // ucSplitLabel1
+            // 
+            this.ucSplitLabel1.AutoSize = true;
+            this.ucSplitLabel1.Font = new System.Drawing.Font("微软雅黑", 13F);
+            this.ucSplitLabel1.LineColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(238)))), ((int)(((byte)(245)))));
+            this.ucSplitLabel1.Location = new System.Drawing.Point(43, 77);
+            this.ucSplitLabel1.MaximumSize = new System.Drawing.Size(0, 25);
+            this.ucSplitLabel1.MinimumSize = new System.Drawing.Size(100, 25);
+            this.ucSplitLabel1.Name = "ucSplitLabel1";
+            this.ucSplitLabel1.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
+            this.ucSplitLabel1.Size = new System.Drawing.Size(100, 25);
+            this.ucSplitLabel1.TabIndex = 1;
+            this.ucSplitLabel1.Text = "账户";
+            this.ucSplitLabel1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // ucSplitLabel2
+            // 
+            this.ucSplitLabel2.AutoSize = true;
+            this.ucSplitLabel2.Font = new System.Drawing.Font("微软雅黑", 13F);
+            this.ucSplitLabel2.LineColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(238)))), ((int)(((byte)(245)))));
+            this.ucSplitLabel2.Location = new System.Drawing.Point(43, 142);
+            this.ucSplitLabel2.MaximumSize = new System.Drawing.Size(0, 25);
+            this.ucSplitLabel2.MinimumSize = new System.Drawing.Size(100, 25);
+            this.ucSplitLabel2.Name = "ucSplitLabel2";
+            this.ucSplitLabel2.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
+            this.ucSplitLabel2.Size = new System.Drawing.Size(100, 25);
+            this.ucSplitLabel2.TabIndex = 1;
+            this.ucSplitLabel2.Text = "密码";
+            this.ucSplitLabel2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // ucTextBoxEx2
+            // 
+            this.ucTextBoxEx2.BackColor = System.Drawing.Color.Transparent;
+            this.ucTextBoxEx2.ConerRadius = 5;
+            this.ucTextBoxEx2.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.ucTextBoxEx2.DecLength = 2;
+            this.ucTextBoxEx2.FillColor = System.Drawing.Color.Empty;
+            this.ucTextBoxEx2.FocusBorderColor = System.Drawing.Color.White;
+            this.ucTextBoxEx2.Font = new System.Drawing.Font("微软雅黑", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.ucTextBoxEx2.InputText = "";
+            this.ucTextBoxEx2.InputType = HZH_Controls.TextInputType.NotControl;
+            this.ucTextBoxEx2.IsFocusColor = true;
+            this.ucTextBoxEx2.IsRadius = true;
+            this.ucTextBoxEx2.IsShowClearBtn = true;
+            this.ucTextBoxEx2.IsShowKeyboard = false;
+            this.ucTextBoxEx2.IsShowRect = false;
+            this.ucTextBoxEx2.IsShowSearchBtn = false;
+            this.ucTextBoxEx2.KeyBoardType = HZH_Controls.Controls.KeyBoardType.UCKeyBorderAll_EN;
+            this.ucTextBoxEx2.Location = new System.Drawing.Point(150, 132);
+            this.ucTextBoxEx2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.ucTextBoxEx2.MaxValue = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            this.ucTextBoxEx2.MinValue = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            -2147483648});
+            this.ucTextBoxEx2.Name = "ucTextBoxEx2";
+            this.ucTextBoxEx2.Padding = new System.Windows.Forms.Padding(5);
+            this.ucTextBoxEx2.PromptColor = System.Drawing.Color.Gray;
+            this.ucTextBoxEx2.PromptFont = new System.Drawing.Font("微软雅黑", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.ucTextBoxEx2.PromptText = "";
+            this.ucTextBoxEx2.RectColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(220)))));
+            this.ucTextBoxEx2.RectWidth = 1;
+            this.ucTextBoxEx2.RegexPattern = "";
+            this.ucTextBoxEx2.Size = new System.Drawing.Size(273, 42);
+            this.ucTextBoxEx2.TabIndex = 0;
+            // 
+            // ucBtnExt1
+            // 
+            this.ucBtnExt1.BackColor = System.Drawing.Color.Transparent;
+            this.ucBtnExt1.BtnBackColor = System.Drawing.Color.Transparent;
+            this.ucBtnExt1.BtnFont = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.ucBtnExt1.BtnForeColor = System.Drawing.Color.White;
+            this.ucBtnExt1.BtnText = "登录";
+            this.ucBtnExt1.ConerRadius = 20;
+            this.ucBtnExt1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.ucBtnExt1.EnabledMouseEffect = false;
+            this.ucBtnExt1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(77)))), ((int)(((byte)(59)))));
+            this.ucBtnExt1.Font = new System.Drawing.Font("微软雅黑", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.ucBtnExt1.IsRadius = true;
+            this.ucBtnExt1.IsShowRect = true;
+            this.ucBtnExt1.IsShowTips = false;
+            this.ucBtnExt1.Location = new System.Drawing.Point(328, 247);
+            this.ucBtnExt1.Margin = new System.Windows.Forms.Padding(0);
+            this.ucBtnExt1.Name = "ucBtnExt1";
+            this.ucBtnExt1.RectColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(77)))), ((int)(((byte)(58)))));
+            this.ucBtnExt1.RectWidth = 1;
+            this.ucBtnExt1.Size = new System.Drawing.Size(95, 40);
+            this.ucBtnExt1.TabIndex = 2;
+            this.ucBtnExt1.TabStop = false;
+            this.ucBtnExt1.TipsColor = System.Drawing.Color.FromArgb(((int)(((byte)(232)))), ((int)(((byte)(30)))), ((int)(((byte)(99)))));
+            this.ucBtnExt1.TipsText = "";
+            // 
+            // textBoxTransparent1
+            // 
+            this.textBoxTransparent1.BackAlpha = 10;
+            this.textBoxTransparent1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.textBoxTransparent1.DecLength = 2;
+            this.textBoxTransparent1.InputType = HZH_Controls.TextInputType.NotControl;
+            this.textBoxTransparent1.Location = new System.Drawing.Point(317, 195);
+            this.textBoxTransparent1.MaxValue = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            this.textBoxTransparent1.MinValue = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            -2147483648});
+            this.textBoxTransparent1.MyRectangle = new System.Drawing.Rectangle(0, 0, 0, 0);
+            this.textBoxTransparent1.Name = "textBoxTransparent1";
+            this.textBoxTransparent1.OldText = null;
+            this.textBoxTransparent1.PromptColor = System.Drawing.Color.Gray;
+            this.textBoxTransparent1.PromptFont = new System.Drawing.Font("微软雅黑", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.textBoxTransparent1.PromptText = "";
+            this.textBoxTransparent1.RegexPattern = "";
+            this.textBoxTransparent1.Size = new System.Drawing.Size(106, 21);
+            this.textBoxTransparent1.TabIndex = 3;
+            // 
+            // ucTextBoxEx3
+            // 
+            this.ucTextBoxEx3.BackColor = System.Drawing.Color.Transparent;
+            this.ucTextBoxEx3.ConerRadius = 5;
+            this.ucTextBoxEx3.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.ucTextBoxEx3.DecLength = 2;
+            this.ucTextBoxEx3.FillColor = System.Drawing.Color.Empty;
+            this.ucTextBoxEx3.FocusBorderColor = System.Drawing.Color.White;
+            this.ucTextBoxEx3.Font = new System.Drawing.Font("微软雅黑", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.ucTextBoxEx3.InputText = "";
+            this.ucTextBoxEx3.InputType = HZH_Controls.TextInputType.NotControl;
+            this.ucTextBoxEx3.IsFocusColor = true;
+            this.ucTextBoxEx3.IsRadius = true;
+            this.ucTextBoxEx3.IsShowClearBtn = true;
+            this.ucTextBoxEx3.IsShowKeyboard = false;
+            this.ucTextBoxEx3.IsShowRect = false;
+            this.ucTextBoxEx3.IsShowSearchBtn = false;
+            this.ucTextBoxEx3.KeyBoardType = HZH_Controls.Controls.KeyBoardType.UCKeyBorderAll_EN;
+            this.ucTextBoxEx3.Location = new System.Drawing.Point(150, 184);
+            this.ucTextBoxEx3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.ucTextBoxEx3.MaxValue = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            this.ucTextBoxEx3.MinValue = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            -2147483648});
+            this.ucTextBoxEx3.Name = "ucTextBoxEx3";
+            this.ucTextBoxEx3.Padding = new System.Windows.Forms.Padding(5);
+            this.ucTextBoxEx3.PromptColor = System.Drawing.Color.Gray;
+            this.ucTextBoxEx3.PromptFont = new System.Drawing.Font("微软雅黑", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.ucTextBoxEx3.PromptText = "";
+            this.ucTextBoxEx3.RectColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(220)))));
+            this.ucTextBoxEx3.RectWidth = 1;
+            this.ucTextBoxEx3.RegexPattern = "";
+            this.ucTextBoxEx3.Size = new System.Drawing.Size(147, 43);
+            this.ucTextBoxEx3.TabIndex = 0;
+            // 
+            // ucSplitLabel3
+            // 
+            this.ucSplitLabel3.AutoSize = true;
+            this.ucSplitLabel3.Font = new System.Drawing.Font("微软雅黑", 13F);
+            this.ucSplitLabel3.LineColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(238)))), ((int)(((byte)(245)))));
+            this.ucSplitLabel3.Location = new System.Drawing.Point(43, 195);
+            this.ucSplitLabel3.MaximumSize = new System.Drawing.Size(0, 25);
+            this.ucSplitLabel3.MinimumSize = new System.Drawing.Size(100, 25);
+            this.ucSplitLabel3.Name = "ucSplitLabel3";
+            this.ucSplitLabel3.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
+            this.ucSplitLabel3.Size = new System.Drawing.Size(100, 25);
+            this.ucSplitLabel3.TabIndex = 1;
+            this.ucSplitLabel3.Text = "验证码";
+            this.ucSplitLabel3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // FrmLog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(475, 337);
+            this.Controls.Add(this.textBoxTransparent1);
+            this.Controls.Add(this.ucBtnExt1);
+            this.Controls.Add(this.ucSplitLabel3);
+            this.Controls.Add(this.ucSplitLabel2);
+            this.Controls.Add(this.ucSplitLabel1);
+            this.Controls.Add(this.ucTextBoxEx3);
+            this.Controls.Add(this.ucTextBoxEx2);
+            this.Controls.Add(this.ucTextBoxEx1);
+            this.Name = "FrmLog";
+            this.Text = "LOG";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private HZH_Controls.Controls.UCTextBoxEx ucTextBoxEx1;
+        private HZH_Controls.Controls.UCSplitLabel ucSplitLabel1;
+        private HZH_Controls.Controls.UCSplitLabel ucSplitLabel2;
+        private HZH_Controls.Controls.UCTextBoxEx ucTextBoxEx2;
+        private HZH_Controls.Controls.UCBtnExt ucBtnExt1;
+        private HZH_Controls.Controls.TextBoxTransparent textBoxTransparent1;
+        private HZH_Controls.Controls.UCTextBoxEx ucTextBoxEx3;
+        private HZH_Controls.Controls.UCSplitLabel ucSplitLabel3;
+    }
+}
+
